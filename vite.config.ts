8171615b5b42239e5ea047ec73f73c1e21659b38
Nandana <nandana.rajan.r@gmail.com>
2026-01-27 23:33:35 +0530

    E

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig(({ mode }) => {
         host: '0.0.0.0',
       },
       plugins: [react()],
-      resolve: {
+      resolve: {   
         alias: {
           '@': path.resolve(__dirname, '.'),
         }
